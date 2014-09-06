08a470c0155cc1651a877d0690cc26890fc4b5dd
ddrocco <keredoccor@gmail.com>
2014-09-06 01:34:18 -0400

    Derek's initial build for the Myo

---
+++ DerekWork/Assets/MyoTrack.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MyoTrack : MonoBehaviour {
+
+	private GameObject Myo;
+	private Vector3 rotation;
+	private Vector3 offset;
+	private int steps = 0;
+	
+	// Use this for initialization
+	void Start () {
+		Myo = GameObject.Find ("Myo");
+	}
+	
+	void Initialize () {
+		offset = Myo.transform.eulerAngles;
+		offset.x += 90;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (steps < 5)	{
+			if (steps == 4) {
+				Initialize();
+			}
+			++steps;
+			return;
+		}
+		rotation = Myo.transform.eulerAngles - offset;
+		rotation.x *= -1;
+		rotation.z = 0;
+		transform.eulerAngles = rotation;
+	}
+}
