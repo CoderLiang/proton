c874fe9e72c321336e10100d586e569dbce62ed7
ddrocco <keredoccor@gmail.com>
2014-09-07 01:28:01 -0400

    FORGOT TO ADD LIKE A TOTAL CLOWN WHO EVEN DOES THAT

---
+++ DerekWork/Assets/DerekScripts/wingflap.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class wingflap : MonoBehaviour {
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (12 * Vector3.forward);
+		//time += Time.deltaTime;
+		//transform.eulerAngles = new Vector3 (5.0f * System.Convert.ToSingle(Mathf.Sin (time)), 0, 0);
+	}
+}
