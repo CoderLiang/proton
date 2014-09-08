0927b52dcbf8061d65de96fc06b1c78e8b6a617d
IanPudney <ipudney@umich.edu>
2014-09-08 15:07:30 -0400

    Removing old files

--- DerekWork/Assets/DerekScripts/Vector3Util.cs
+++
@@ -1,9 +0,0 @@
-using System;
-using UnityEngine;
-public class Vector3Util
-{
-	public static Vector3 Vector3(double x, double y, double z)
-	{
-		return new Vector3(System.Convert.ToSingle(x), System.Convert.ToSingle (y), System.Convert.ToSingle (z));
-	}
-}
